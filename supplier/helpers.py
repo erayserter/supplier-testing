c17d82ea07b3c266683b7d7d238f9b5d29711535
Eray Serter <erayserter@Erays-MacBook-Pro.local>
2023-03-07 13:23:53 +0300

    added new test cases

--- supplier/helpers.py
+++ supplier/helpers.py
@@ -3,17 +3,13 @@ from rest_framework import status
 import requests
 
 
-def bad_request(test_case, message, post=False):
+def bad_request(test_case, post=None):
     response = request_with_data(test_case, post)
     # TODO: this will be response.data after request will be implemented with Client class.
-    response_data = response.json()
-
     test_case.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    test_case.assertFalse(response_data.get('success'))
-    test_case.assertEqual(response_data.get("message"), message)
 
 
-def request_with_data(test_case, post=False):
+def request_with_data(test_case, post=None):
     # TODO: this will be implemented with self.client.get and post.
     if post:
         return requests.post(test_case.url, json=test_case.data)
--- supplier/tests.py
+++ supplier/tests.py
@@ -3,17 +3,14 @@ from django.db.models import Q
 
 from rest_framework import status
 
-from supplier.models import SupplierApplication, AssociatedSupplierDealerships
-from supplier.initial_data import CREATE_APPLICATION_DATA, STATUS_DATA, UPDATE_DATA
 from supplier.helpers import bad_request, request_with_data
 
+import json
 
-AMOUNT_BOUNDARY = 9999
 
-
-class CreateSupplierApplicationTests(TestCase):
+class CreateSupplierApplicationTest(TestCase):
     def setUp(self):
-        self.data = CREATE_APPLICATION_DATA.copy()
+        self.data = json.load(open('supplier/datas/application_data.json', 'r'))
         self.url = "https://dev.bumper.co.uk/core/api/supplier/application/v1/"
 
     def test_valid_data(self):
@@ -22,51 +19,131 @@ class CreateSupplierApplicationTests(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue(data.get('success'))
-        self.assertEqual(data.get('message'), "Application created successfully")
         self.assertIsNotNone(data.get('data').get('token'))
         self.assertIsNotNone(data.get('data').get('redirect_url'))
 
+    def test_valid_data_with_preapproval_link(self):
+        del self.data['amount']
+        self.data['preapproval_link'] = 1
+        self.data['signature'] = 'acd740069543c3d2501b3fc2e7e155f90b2f2745b1cb93a2cf059719861bee0e'
+        self.test_valid_data()
+
+    def test_valid_data_without_preapproval_link(self):
+        self.data['preapproval_link'] = 0
+        self.data['signature'] = '2cf31460cc223ffb5bb9c4e9ce347d1eae6a7a46ff8e0bf30b93e06d01e1e072'
+        bad_request(self, post=True)
+
+    def test_without_api_key(self):
+        del self.data['api_key']
+        bad_request(self, post=True)
+
     def test_invalid_api_key(self):
         self.data['api_key'] = '123'
-        bad_request(self, "API Key provided is Invalid", post=True)
+        bad_request(self, post=True)
+
+    def test_without_signature(self):
+        del self.data['signature']
+        bad_request(self, post=True)
 
     def test_invalid_signature(self):
         self.data['signature'] = '123'
-        bad_request(self, "Signature provided is Invalid", post=True)
+        bad_request(self, post=True)
 
     def test_amount_gt_boundary(self):
-        self.data['amount'] = f"{AMOUNT_BOUNDARY + 1}.00"
-        self.data['signature'] = "488d309ab3dc22d507571920b758f42cec8ad99aa3c4c5e3e63a159a83b7cb1b"
-        bad_request(self, "Amount provided is Invalid", post=True)
+        MAX_AMOUNT = 5000
+        self.data['amount'] = MAX_AMOUNT + 1
+        self.data['signature'] = "3d2479a6ac181a8eb4d8dca019010d9f792d598975cacdf54a649eff9d73e3d2"
+        bad_request(self, post=True)
+
+    def test_amount_lt_boundary(self):
+        MIN_AMOUNT = 1
+        self.data['amount'] = MIN_AMOUNT - 1
+        self.data['signature'] = "cdf0ea73558d322a85fb03910df8314e8d88ab3870cc0f4af6e7ae5cd81a3d32"
+        bad_request(self, post=True)
 
     def test_invalid_currency(self):
         self.data['currency'] = 'TL'
         self.data['signature'] = '3dadec9011b16d1fe4f1cccea9ab9a9f49aa0e41bda1ae4ca9dec8b95abfcabf'
-        bad_request(self, "Currency provided is Invalid", post=True)
+        bad_request(self, post=True)
 
     def test_empty_product_description(self):
         self.data['product_description'] = []
-        bad_request(self, "Product Description provided is Invalid", post=True)
+        bad_request(self, post=True)
+
+    def test_without_item_product_description(self):
+        self.data['product_description'] = [{
+            "quantity": "2",
+            "price": "150.00"
+        }]
+        bad_request(self, post=True)
+
+    def test_without_quantity_product_description(self):
+        self.data['product_description'] = [{
+            "item": "Pirelli Cinturato P7 Tyre",
+            "price": "150.00"
+        }]
+        bad_request(self, post=True)
+
+    def test_without_price_product_description(self):
+        self.data['product_description'] = [{
+            "item": "Pirelli Cinturato P7 Tyre",
+            "quantity": "2",
+        }]
+        bad_request(self, post=True)
+
+    def test_invalid_item_product_description(self):
+        self.data['product_description'] = [{
+            "item": "invalid product item name",
+            "quantity": "2",
+            "price": "150.00"
+        }]
+        bad_request(self, post=True)
+
+    def test_national_id_number_len_gt_boundary(self):
+        self.data['national_id_number'] = '0123456789101112'  # TODO: length of the id number > 15
+        self.data['signature'] = 'c69c429a412bfe75e7d6639d661ac5471d5592bdcf00478e7220f6fbf8b490eb'
+        bad_request(self, post=True)
+
+    def test_national_id_number(self):
+        self.data['national_id_number'] = '123456789101112'
+        self.data['signature'] = '3ceb26ee447dba2aac852f754fc800f19b070fb0b1f45dd97f44d44cf6ee1e6b'
+        self.test_valid_data()
+
+    def test_send_email(self):
+        self.data['send_email'] = True
+        self.data['signature'] = '9d46e8f08f0dd402704627443f350822aeba7adb64229e3d8070efeab3d013b6'
+        self.test_valid_data()
+
+    def test_send_sms(self):
+        self.data['send_sms'] = True
+        self.data['signature'] = '12e0436d9d884040041b56279e8a860e37588b824a1592624520305b0b67f129'
+        self.test_valid_data()
+
+    def test_send_email_and_sms(self):
+        self.data['send_email'] = True
+        self.data['send_sms'] = True
+        self.data['signature'] = 'bf50484614ad3f4f1455bd08c2d8cd7cfd38d6016d63b1b54590d042bacb5caa'
+        self.test_valid_data()
 
     def test_invalid_email(self):
         self.data['email'] = 'invalid-email'
         self.data['signature'] = 'ad88d53e638a5cc3673c96487943df99b9dcdf5881756f7abd9cbbba6f3af45c'
-        bad_request(self, "Email provided is Invalid", post=True)
+        bad_request(self, post=True)
 
     def test_invalid_product_id(self):
         self.data['product_id'] = '0'
-        self.data['signature'] = 'fdbf117c629083952c0b3c9d8a4fc587f3a763852c9d0048e217616976706853'
-        bad_request(self, "Product ID is Invalid", post=True)
+        self.data['signature'] = '4d423521d19e9f15f454723c2fd2dc7f067da9d9dbcf5f977f4ee637fcacd593'
+        bad_request(self, post=True)
 
     def test_invalid_mobile(self):
         self.data['mobile'] = ''
         self.data['signature'] = '8b8c04df32ff4417a4c1affcbae60c46d64cea28ed8ca8ebaec1a3667e5295b9'
-        bad_request(self, 'mobile not provided correctly', post=True)
+        bad_request(self, post=True)
 
 
-class StatusSupplierApplicationTests(TestCase):
+class StatusSupplierApplicationTest(TestCase):
     def setUp(self):
-        self.data = STATUS_DATA.copy()
+        self.data = json.load(open('supplier/datas/status_data.json', 'r'))
         self.url = "https://dev.bumper.co.uk/core/api/supplier/status/v1/"
 
     def test_valid_data(self):
@@ -76,8 +153,6 @@ class StatusSupplierApplicationTests(TestCase):
         self.assertEqual(data.get('success'), True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsNotNone(data.get('data'))
-        self.assertIsNotNone(data.get('data').get('token'))
-        self.assertIsNotNone(data.get('data').get('customer_reference'))
 
     def test_valid_data_customer_reference(self):
         del self.data['token']
@@ -85,23 +160,37 @@ class StatusSupplierApplicationTests(TestCase):
         self.data['signature'] = '49b5204dd21cad2eca7acdfbc46271b8deeb307cbd6e28dc085d0a1f4cf33847'
         self.test_valid_data()
 
+    def test_invalid_customer_reference(self):
+        del self.data['token']
+        self.data['bumper_reference'] = 0
+        self.data['signature'] = 'a412adc993169bb31d40572a5a472d921da0aa9357e3d5dfee6852d65200c3c4'
+        bad_request(self)
+
+    def test_without_signature(self):
+        del self.data['signature']
+        bad_request(self)
+
     def test_invalid_signature(self):
         self.data['signature'] = 'e768335bf3f3f7f75a532745ac0cb6af0bd5294fa26627b1b565c77aa516cbfc'
-        bad_request(self, 'Signature provided is Invalid')
+        bad_request(self)
+
+    def test_without_api_key(self):
+        del self.data['api_key']
+        bad_request(self)
 
     def test_invalid_api_key(self):
         self.data['api_key'] = '123'
-        bad_request(self, 'API Key provided is Invalid')
+        bad_request(self)
 
     def test_invalid_token(self):
         self.data['token'] = '123'
         self.data['signature'] = '3fbb67838aa67f9ab4f9264d9847e62add7d55d3f826f82fdbc7eaa273244949'
-        bad_request(self, "Token provided is Invalid")
+        bad_request(self)
 
 
-class UpdateApplicationTests(TestCase):
+class UpdateApplicationTest(TestCase):
     def setUp(self):
-        self.data = UPDATE_DATA.copy()
+        self.data = json.load(open('supplier/datas/update_data.json', 'r'))
         self.url = "https://dev.bumper.co.uk/core/api/supplier/update/v1/"
 
     def test_valid_data(self):
@@ -110,7 +199,6 @@ class UpdateApplicationTests(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(data.get('success'), True)
-        self.assertEqual(data.get('message'), '')
 
     def test_valid_data_bumper_reference(self):
         self.data['bumper_reference'] = "112867"
@@ -118,20 +206,30 @@ class UpdateApplicationTests(TestCase):
         del self.data['token']
         self.test_valid_data()
 
+    def test_invalid_bumper_reference(self):
+        self.data['bumper_reference'] = "0"
+        self.data['signature'] = '4f6a5b81fba4bf83e534aca06ae99356b2812c0cf3caea217b86459ae06c5581'
+        del self.data['token']
+
     def test_invalid_signature(self):
         self.data['signature'] = 'e768335bf3f3f7f75a532745ac0cb6af0bd5294fa26627b1b565c77aa516cbfc'
-        bad_request(self, 'Signature provided is Invalid', post=True)
+        bad_request(self, post=True)
 
     def test_invalid_api_key(self):
         self.data['api_key'] = '123'
-        bad_request(self, 'API Key provided is Invalid', post=True)
+        bad_request(self, post=True)
 
     def test_invalid_token(self):
         self.data['token'] = '123'
         self.data['signature'] = 'e967dbcfc9e49fc4879ad84f9302772862891ebda7a48a985c2019da559a5ffb'
-        bad_request(self, "Token provided is invalid.", post=True)
+        bad_request(self, post=True)
 
-    def test_invalid_amount(self):
-        self.data['amount'] = AMOUNT_BOUNDARY + 1
-        self.data['signature'] = 'faf60089cd887fbf345907b3ca677be3a0fa64f38588f7ec5ba1f090df21b88e'
-        bad_request(self, "Amount provided is Invalid", post=True)
+    def test_amount_gt_boundary(self):
+        self.data['amount'] = "5001"
+        self.data['signature'] = "24644e4c9efdf96dcb1a33713613f7db9802d78daffa59bd05d6ec6cd11faa2a"
+        bad_request(self, post=True)
+
+    def test_amount_lt_boundary(self):
+        self.data['amount'] = "0"
+        self.data['signature'] = "e8e88f4001702fc92607b6ff0273915c5591fbcab15b0552cf915cb3ab558f2b"
+        bad_request(self, post=True)
